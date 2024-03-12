0e2c8eab6ba2aa60ffce2344c28cd9379e5a6e67
emrgnt-cmplxty <68796651+emrgnt-cmplxty@users.noreply.github.com>
2024-03-11 22:47:40 -0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -4,5 +4,5 @@ from r2r.pipelines import WebSearchRAGPipeline
 
 # Creates a pipeline using the `WebSearchRAGPipeline` implementation
 app = E2EPipelineFactory.create_pipeline(
-    rag_pipeline_impl=WebSearchRAGPipeline
+    rag_pipeline_impl=WebSearchRAGPipeline("config.json")
 )
